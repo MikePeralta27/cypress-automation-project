d4b475fdd958fe212908163157d28adf45e069d7
Michael Peralta <peralta.michael27@gmail.com>
2025-04-06 21:48:04 -0400

    Add product test and page methods also checkout page

--- cypress/e2e/productTest.cy.js
+++ cypress/e2e/productTest.cy.js
@@ -1,26 +1,53 @@
 import LoginPage from "../pageObjects/loginPage";
 import data from "../fixtures/data.json";
 import ProductPage from "../pageObjects/productPage";
+import CheckoutPage from "../pageObjects/checkoutPage";
 
-const loginPage = new LoginPage();
+const loginPage = new LoginPage();    
 const productPage = new ProductPage();
+const checkoutPage = new CheckoutPage();
 
 describe('Product page test', () => {
   
     beforeEach(() => {
       
       loginPage.goToLoginPage();
+      loginPage.login(data.standard_user, data.password);
+
     });
 
   it('Add product to cart', () => {
-    loginPage.login(data.standard_user, data.password);
-    productPage.getProductTitle().should('be.visible');
-    productPage.getProductTitle().should('have.text', 'Products');
-    productPage.getProductContainer().should('be.visible');
+    
     productPage.addToCart(0);
     productPage.getRemoveButton().should('be.visible');
     productPage.checkCartBadge(1);
     productPage.goToCart();
+    productPage.getCartBadge().should('be.visible');
+    productPage.getCartBadge().should('have.text', '1');
+    
+
+  });
+
+  it('Add multiple products to cart', () => {
+    productPage.addToCart(0);
+    productPage.addToCart(1);
+    productPage.addToCart(2);
+    productPage.checkCartBadge(3);
+    productPage.getCartBadge().should('be.visible');
+    productPage.getCartBadge().should('have.text', '3');
+    productPage.getInventoryItem(0).should('be.visible');
+    productPage.getInventoryItem(1).should('be.visible');
+    productPage.getInventoryItem(2).should('be.visible');
+  });
+
+
+  it('Remove product from cart', () => {
+    productPage.addToCart(0);
+    productPage.getCartBadge().should('be.visible');
+    productPage.getCartBadge().should('have.text', '1');
+    productPage.removeProduct(0);
+    productPage.getCartBadge().should('not.exist');
+    productPage.getRemoveButton().should('not.exist');
 
   });
 
---
+++ cypress/pageObjects/checkoutPage.js
@@ -0,0 +1,70 @@
+import ProductPage from "./productPage";
+const productPage = new ProductPage();
+
+export default class CheckoutPage {
+    constructor() {
+        this.checkoutButton = '[data-test="checkout"]'
+        this.firstNameInput = '[data-test="firstName"]'
+        this.lastNameInput = '[data-test="lastName"]'
+        this.postalCodeInput = '[data-test="postalCode"]'
+        this.cartItem = '[data-test="inventory-item"]'
+        this.continueButton = '[data-test="continue"]'
+        this.cancelButton = '[data-test="cancel"]'
+        this.removeButton = '[data-test*="remove"]'
+    }
+
+    getCartItem(index) {
+        return cy.get(this.cartItem).eq(index)
+    }
+
+    
+    getCheckoutButton() {
+        return cy.get(this.checkoutButton)
+    }
+
+    getFirstNameInput() {
+        return cy.get(this.firstNameInput)
+    }
+
+    getLastNameInput() {
+        return cy.get(this.lastNameInput)
+    }
+
+    getPostalCodeInput() {
+        return cy.get(this.postalCodeInput)
+    }
+
+    getContinueButton() {
+        return cy.get(this.continueButton)
+    }
+
+    getCancelButton() {
+        return cy.get(this.cancelButton)
+    }
+
+    getRemoveButton() {
+        return cy.get(this.removeButton)
+    }
+
+    fillCheckoutForm(firstName, lastName, postalCode) {
+        this.getFirstNameInput().type(firstName)
+        this.getLastNameInput().type(lastName)
+        this.getPostalCodeInput().type(postalCode)
+    }
+
+    removeFromCart(index) {
+        this.getCartItem(index).find(this.removeButton).click()
+    }
+
+    continueCheckout() {
+        this.getContinueButton().click()
+    }   
+
+    cancelCheckout() {
+        this.getCancelButton().click()
+    }
+    
+    
+    
+    
+}
