71e76a891119c7d07b8c816a433b22dd3e0edd37
Michael Peralta <peralta.michael27@gmail.com>
2025-04-14 00:51:37 -0400

    Add first checkout test

---
+++ cypress/e2e/checkoutTest.cy.js
@@ -0,0 +1,26 @@
+import LoginPage from "../pageObjects/loginPage";
+import data from "../fixtures/data.json";
+import ProductPage from "../pageObjects/productPage";
+
+const loginPage = new LoginPage();
+const productPage = new ProductPage();
+
+describe('Login page test', () => {
+  
+    beforeEach(() => {
+      
+      loginPage.goToLoginPage();
+    });
+
+
+  it('Checkout process', () => {
+    loginPage.login(Cypress.env('standard_user'), Cypress.env('password'));
+    productPage.getProductTitle().should('be.visible');
+    productPage.getProductTitle().should('have.text', 'Products');
+    productPage.getProductContainer().should('be.visible');
+    cy.get('.title').should('have.text', 'Products');
+    productPage.logout();
+    loginPage.getLoginButton().should('be.visible');
+  });
+
+});
