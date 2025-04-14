afbcf82932a6de0e766e88939489f71b24190cde
Michael Peralta <peralta.michael27@gmail.com>
2025-04-14 00:38:22 -0400

    Add CI to run Cypress tests on push

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
