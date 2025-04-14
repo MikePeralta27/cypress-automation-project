3da0cf91f971958d9a4a3f473404ed3f2160ef6d
Michael Peralta <peralta.michael27@gmail.com>
2025-04-14 00:24:03 -0400

    Refactor page objects

--- cypress/pageObjects/checkoutPage.js
+++ cypress/pageObjects/checkoutPage.js
@@ -3,65 +3,65 @@ const productPage = new ProductPage();
 
 export default class CheckoutPage {
     constructor() {
-        this.checkoutButton = '[data-test="checkout"]'
-        this.firstNameInput = '[data-test="firstName"]'
-        this.lastNameInput = '[data-test="lastName"]'
-        this.postalCodeInput = '[data-test="postalCode"]'
-        this.cartItem = '[data-test="inventory-item"]'
-        this.continueButton = '[data-test="continue"]'
-        this.cancelButton = '[data-test="cancel"]'
-        this.removeButton = '[data-test*="remove"]'
+        this.checkoutButton = '[data-test="checkout"]';
+        this.firstNameInput = '[data-test="firstName"]';
+        this.lastNameInput = '[data-test="lastName"]';
+        this.postalCodeInput = '[data-test="postalCode"]';
+        this.cartItem = '[data-test="inventory-item"]';
+        this.continueButton = '[data-test="continue"]';
+        this.cancelButton = '[data-test="cancel"]';
+        this.removeButton = '[data-test*="remove"]';
     }
 
     getCartItem(index) {
-        return cy.get(this.cartItem).eq(index)
+        return cy.get(this.cartItem).eq(index);
     }
 
     
     getCheckoutButton() {
-        return cy.get(this.checkoutButton)
+        return cy.get(this.checkoutButton);
     }
 
     getFirstNameInput() {
-        return cy.get(this.firstNameInput)
+        return cy.get(this.firstNameInput);
     }
 
     getLastNameInput() {
-        return cy.get(this.lastNameInput)
+        return cy.get(this.lastNameInput);
     }
 
     getPostalCodeInput() {
-        return cy.get(this.postalCodeInput)
+        return cy.get(this.postalCodeInput);
     }
 
     getContinueButton() {
-        return cy.get(this.continueButton)
+        return cy.get(this.continueButton);
     }
 
     getCancelButton() {
-        return cy.get(this.cancelButton)
+        return cy.get(this.cancelButton);
     }
 
     getRemoveButton() {
-        return cy.get(this.removeButton)
+        return cy.get(this.removeButton);
     }
 
     fillCheckoutForm(firstName, lastName, postalCode) {
-        this.getFirstNameInput().type(firstName)
-        this.getLastNameInput().type(lastName)
-        this.getPostalCodeInput().type(postalCode)
+        this.getFirstNameInput().type(firstName);
+        this.getLastNameInput().type(lastName);
+        this.getPostalCodeInput().type(postalCode);
     }
 
     removeFromCart(index) {
-        this.getCartItem(index).find(this.removeButton).click()
+        this.getCartItem(index).find(this.removeButton).click();
     }
 
     continueCheckout() {
-        this.getContinueButton().click()
+        this.getContinueButton().click();
     }   
 
     cancelCheckout() {
-        this.getCancelButton().click()
+        this.getCancelButton().click();     
     }
     
     
--- cypress/pageObjects/loginPage.js
+++ cypress/pageObjects/loginPage.js
@@ -3,50 +3,47 @@ const productPage = new ProductPage();
 
 export default class LoginPage {
     constructor() {
-        this.usernameInput = '#user-name'
-        this.passwordInput = '#password'
-        this.loginButton = '#login-button'
-        this.errorMessage = '#login_button_container > div > form > div.error-message-container.error > h3'
+        this.usernameInput = '#user-name';
+        this.passwordInput = '#password';
+        this.loginButton = '#login-button';
+        this.errorMessage = '#login_button_container > div > form > div.error-message-container.error > h3';
 
     }
 
         getUsernameInput() {
-            return cy.get(this.usernameInput)
+            return cy.get(this.usernameInput);
         }
 
         getPasswordInput() {
-            return cy.get(this.passwordInput)
+            return cy.get(this.passwordInput);
         }
 
         getLoginButton() {
-            return cy.get(this.loginButton)
+            return cy.get(this.loginButton);
         }
 
         getErrorMessage() {
-            return cy.get(this.errorMessage)
+            return cy.get(this.errorMessage);
         }
 
         goToLoginPage() {
-            cy.visit('https://www.saucedemo.com/')
+            cy.visit('https://www.saucedemo.com/');
         }
 
         login(username, password) {
-            this.getUsernameInput().type(username)
-            this.getPasswordInput().type(password)
-            this.getLoginButton().click()
-            productPage.getProductTitle().should('be.visible')
+            this.getUsernameInput().type(username);
+            this.getPasswordInput().type(password);
+            this.getLoginButton().click();
         }
 
         loginWithEmptyCredentials() {
-            this.getLoginButton().click()
+            this.getLoginButton().click();
         }
 
         loginWithOnlyUsername() {
-            this.getUsernameInput().type(Cypress.env('standard_user'))
-            this.getLoginButton().click()
+            this.getUsernameInput().type(Cypress.env('standard_user'));
+            this.getLoginButton().click();
         }
-        
-
 
 }
 
