624191bc271540857ed00e7cad452ca4518772fb
Michael Peralta <peralta.michael27@gmail.com>
2025-04-16 07:29:30 +0000

    Update checkout test

--- cypress/e2e/checkoutTest.cy.js
+++ cypress/e2e/checkoutTest.cy.js
@@ -20,7 +20,7 @@ describe('Login page test', () => {
     productPage.getProductContainer().should('be.visible');
     cy.get('.title').should('have.text', 'Products');
     productPage.logout();
-    loginPage.getLoginButton().should('be.not.visible');
+    loginPage.getLoginButton().should('be.visible');
   });
 
 });ß
